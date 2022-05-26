53a31de83610d0da0880fe821cd9b11ff4d260d9
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-05-26 11:23:59 -0300

    Criado metodo de atualização automática do app

--- Templates/App.js
+++ Templates/App.js
@@ -5,12 +5,14 @@ import { Settings } from "../Settings/Settings.js";
 import { Component } from "../Components/Components.js";
 import { Item } from "../Class/Item.js";
 import { List } from "../Class/List.js";
+import { Utils } from "../Utils/Utils.js";
 
 export class App {
     listObject;
     itemObject = new Item;
+    utils = new Utils;
 
-    constructor(){
+    constructor() {
         let data = this.loadInformation();
         if (data) { this.listObject = new List(data) }
     }
@@ -22,14 +24,15 @@ export class App {
         let value = new ValueMoney;
         let settingsJson = new Settings;
 
-        item.template('#addItemHeader', settingsJson, components,this.listObject,this.itemObject);
-        listItems.template('#listItensSection',settingsJson, this.listObject, components,this.steepTitleValue);
+        item.template('#addItemHeader', settingsJson, components, this.listObject, this.itemObject);
+        listItems.template('#listItensSection', settingsJson, this.listObject, components, this.steepTitleValue);
         value.Tempalte('#calcValueFooter', settingsJson, components)
 
         this.settings();
+        this.updateAPP();
     }
     settings() {
-        document.getElementById('valueMoneyMax').onchange = () => { 
+        document.getElementById('valueMoneyMax').onchange = () => {
             this.listObject.setValueMax(document.getElementById('valueMoneyMax').value);
             this.steepTitleValue();
         };
@@ -38,22 +41,26 @@ export class App {
             this.listObject.reloadList();
         };
         this.listObject.reloadValueTotal();
-        
     }
     loadInformation() {
         let list = {
             list: [],
-            maxValue:0
+            maxValue: 0
         }
-        if (!localStorage.getItem('mercurio_list')) {
-            localStorage.setItem('mercurio_list', JSON.stringify(list))
-        }
+        !localStorage.mercurio_list && localStorage.setItem('mercurio_list', JSON.stringify(list))
+        !localStorage.mercurio_update && localStorage.setItem('mercurio_update', this.utils.toDay());
         return this.itemObject.loadItems(JSON.parse(localStorage.getItem('mercurio_list')))
     }
-    steepTitleValue(){
-        document.getElementById('valueMoneyMax').value ?  
-        document.querySelector("#divVlrTotal label b").innerText = "Valor Restante:"
-        :
-        document.querySelector("#divVlrTotal label b").innerText = "Valor Total:"
+    steepTitleValue() {
+        document.getElementById('valueMoneyMax').value ?
+            document.querySelector("#divVlrTotal label b").innerText = "Valor Restante:"
+            :
+            document.querySelector("#divVlrTotal label b").innerText = "Valor Total:"
+    }
+    updateAPP(){
+        if (navigator.onLine && (this.utils.toDay() != localStorage.mercurio_update)) {
+            window.location.reload();
+            localStorage.setItem('mercurio_update', this.utils.toDay())
+        }
     }
 }
---
+++ Utils/Utils.js
@@ -0,0 +1,10 @@
+export class Utils{
+    toDay() {
+        let data = new Date();
+        let day = String(data.getDate()).padStart(2, '0');
+        let month = String(data.getMonth() + 1).padStart(2, '0');
+        let year = data.getFullYear();
+        let hours = data.getHours() + ':' + data.getMinutes()
+        return  year + '-' + month + '-' + day+ '-'+hours;
+    }
+}
